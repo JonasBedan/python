ecaf967d201b60285b4153ba5cecedef30ecb889
JonasBedan <jonas.bedan@gmail.com>
2026-01-27 20:30:08 +0100

    Update reaction_time.py

--- reaction_time/reaction_time.py
+++ reaction_time/reaction_time.py
@@ -3,11 +3,12 @@ import time
 from PIL import ImageGrab
 
 while True:
-    x,y = pyautogui.position()
+    x,y = 1495, 200
     img = ImageGrab.grab(bbox=(x,y,x+1,y+1))
     color = img.getpixel((0,0))
     print(f"X:{x} Y:{y} RGB:{color}")
     if(color == (75, 219, 106)):
+       pyautogui.dragTo(x,y)
        pyautogui.click()
        break
 
